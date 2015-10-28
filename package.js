ff0cb0512ba5e57b684f72e71dc65105dc0893d2
spectrum <spectrick@gmail.com>
2015-10-29 00:52:15 +0900

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'spectrum:kakao',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Kakao OAuth flow',
   git: 'https://github.com/acidsound/meteor-kakao',
   documentation: 'README.md'
